be35aab4d41a5e523bc0565597a76913c88aa79d
hana <hana.ivancic2@gmail.com>
2023-11-16 10:37:50 +0100

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    resources :classrooms
+    resources :students
+    resources :teachers
+    resources :decks
+    resource :questions
+    resource :schools
+    resource :statistics
+  end
 end
